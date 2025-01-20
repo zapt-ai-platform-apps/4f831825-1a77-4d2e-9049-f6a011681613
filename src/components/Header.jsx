accfe227314237a2a987ba3420d6faac5c49ba86
David Fox <davidmfox123@gmail.com>
2025-01-20 22:40:55 +0000

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ function Header({ menuOpen, setMenuOpen }) {
         <img
           src="https://supabase.zapt.ai/storage/v1/render/image/public/icons/4f831825-1a77-4d2e-9049-f6a011681613/599b08f5-e3d4-498e-a2a5-687a781d184a.png"
           alt="UpGrade Logo"
-          className="w-20 h-20"
+          className="w-24 h-18"
         />
       </div>
       <div className="sm:hidden">
